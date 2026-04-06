48eee808505887cf7e28e4f4c99a45e678cf8153
lalatina11 <rahmadancandra@gmail.com>
2026-04-06 09:19:26 +0700

    first setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
+	import { Toaster } from '$lib/components/ui/sonner';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+{@render children()}
+<ModeWatcher />
+<Toaster position="top-center" />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+	import { toast } from 'svelte-sonner';
+</script>
+
+<svelte:head>
+	<title>My CRM</title>
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Button size="default" onclick={() => toast.success('Test')}>Toast</Button>
