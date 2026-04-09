e263a39fb58edffb0416f4e81aa9f48ccaf555e0
lalatina11 <rahmadancandra@gmail.com>
2026-04-09 19:20:18 +0700

    style: dialog width adjustmen

--- src/routes/dashboard/contacts/_components/add-contact-form.svelte
+++ src/routes/dashboard/contacts/_components/add-contact-form.svelte
@@ -65,7 +65,7 @@
 
 <Dialog.Root open={isDialogOpen} onOpenChange={() => setDialogOpen()}>
 	<Button onclick={() => setDialogOpen('open')}><Plus /> Add Contact</Button>
-	<Dialog.Content class="w-lg">
+	<Dialog.Content class="w-full md:w-[70vw] lg:w-lg">
 		<form onsubmit={handleSubmit} class="space-y-3">
 			<Dialog.Header>
 				<Dialog.Title>Add Contact</Dialog.Title>
