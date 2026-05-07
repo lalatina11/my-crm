6cd75a3e64ea122bf5a85538185a823f813485ef
lalatina11 <rahmadancandra@gmail.com>
2026-05-07 15:28:50 +0700

    fix: image validation is real

--- src/lib/validators/user-schema.ts
+++ src/lib/validators/user-schema.ts
@@ -3,7 +3,20 @@ import { z } from "zod";
 export const profileSchema = z.object({
 	name: z.string().min(3, "Name must be at least 3 characters"),
 	email: z.email("Invalid email address"),
-	image: z.any().nullable(),
+	image: z
+		.any()
+		.nullable()
+		.refine((input) => input instanceof File, "Only image are allowed")
+		.refine((input) => {
+			if (input instanceof File) {
+				return input.type.startsWith("image");
+			}
+		}, "Only Image are allowed")
+		.refine((input) => {
+			if (input instanceof File) {
+				return input.size < 5 * 1024 * 1024;
+			}
+		}, "Max image size is 5MB"),
 });
 
 export type ProfileSchema = z.infer<typeof profileSchema>;
