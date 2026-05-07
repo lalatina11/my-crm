33f388a5216d3013d8df7488fb20006b199738d7
lalatina11 <rahmadancandra@gmail.com>
2026-05-07 09:16:52 +0700

    feat: user menu on sidebar button

--- src/lib/components/dashboad-sidebar.svelte
+++ src/lib/components/dashboad-sidebar.svelte
@@ -1,34 +1,39 @@
 <script lang="ts">
 	/* eslint-disable svelte/no-navigation-without-resolve */
-	import { page } from '$app/state';
-	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import type { DashboardSidebarMenuItem } from '$lib/types/dashboard-sidebar-menu-item';
-	import { Activity, Contact, HeartHandshake, LayoutDashboard } from '@lucide/svelte/icons';
-	import { buttonVariants } from './ui/button';
-	import { resolve } from '$app/paths';
+	import { page } from "$app/state";
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import type { DashboardSidebarMenuItem } from "$lib/types/dashboard-sidebar-menu-item";
+	import { Activity, Contact, HeartHandshake, LayoutDashboard, User } from "@lucide/svelte/icons";
+	import { buttonVariants } from "./ui/button";
+	import { resolve } from "$app/paths";
+	import UserDropdown from "./auth/user-dropdown.svelte";
+	import * as Avatar from "./ui/avatar";
+	import { authClient } from "$lib/hooks/auth-client";
 
 	const items = [
 		{
-			title: 'Dashboard',
-			url: resolve('/dashboard'),
-			icon: LayoutDashboard
+			title: "Dashboard",
+			url: resolve("/dashboard"),
+			icon: LayoutDashboard,
 		},
 		{
-			title: 'Contacts',
-			url: resolve('/dashboard/contacts'),
-			icon: Contact
+			title: "Contacts",
+			url: resolve("/dashboard/contacts"),
+			icon: Contact,
 		},
 		{
-			title: 'Deals',
-			url: resolve('/dashboard/deals'),
-			icon: HeartHandshake
+			title: "Deals",
+			url: resolve("/dashboard/deals"),
+			icon: HeartHandshake,
 		},
 		{
-			title: 'Activities',
-			url: resolve('/dashboard/activities'),
-			icon: Activity
-		}
+			title: "Activities",
+			url: resolve("/dashboard/activities"),
+			icon: Activity,
+		},
 	] satisfies Array<DashboardSidebarMenuItem>;
+
+	const session = authClient.useSession();
 </script>
 
 <Sidebar.Root collapsible="icon">
@@ -45,8 +50,8 @@
 							<Sidebar.MenuItem>
 								<Sidebar.MenuButton
 									class={buttonVariants({
-										variant: item.url === page.url.pathname ? 'default' : 'ghost',
-										class: 'items-center justify-start'
+										variant: item.url === page.url.pathname ? "default" : "ghost",
+										class: "items-center justify-start",
 									})}
 								>
 									<item.icon />
@@ -59,4 +64,21 @@
 			</Sidebar.GroupContent>
 		</Sidebar.Group>
 	</Sidebar.Content>
+	<Sidebar.Footer>
+		<Sidebar.Menu>
+			<UserDropdown>
+				<Sidebar.MenuItem>
+					<Sidebar.MenuButton>
+						<Avatar.Root class="size-5">
+							<Avatar.Image src={$session.data?.user.image} />
+							<Avatar.Fallback>
+								<User />
+							</Avatar.Fallback>
+						</Avatar.Root>
+						<span> {$session.data?.user.name}</span>
+					</Sidebar.MenuButton>
+				</Sidebar.MenuItem>
+			</UserDropdown>
+		</Sidebar.Menu>
+	</Sidebar.Footer>
 </Sidebar.Root>
