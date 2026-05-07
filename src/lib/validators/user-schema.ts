27cf7ccadb1245e4a86c987c6e0b94baa5f6058f
lalatina11 <rahmadancandra@gmail.com>
2026-05-07 14:53:59 +0700

    fix: name min 3 chars

--- src/lib/validators/user-schema.ts
+++ src/lib/validators/user-schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const profileSchema = z.object({
-	name: z.string().min(2, "Name must be at least 2 characters"),
+	name: z.string().min(3, "Name must be at least 3 characters"),
 	email: z.email("Invalid email address"),
 	image: z.any().nullable(),
 });
