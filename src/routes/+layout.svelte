94bf6476d667d2412f77f82b0aaf1e607282195e
lalatina11 <rahmadancandra@gmail.com>
2026-05-07 13:38:47 +0700

    feat: separate shadcn css with layout.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
-	import { ModeWatcher } from 'mode-watcher';
-	import { Toaster } from '$lib/components/ui/sonner';
+	import "./layout.css";
+	import favicon from "$lib/assets/favicon.svg";
+	import { ModeWatcher } from "mode-watcher";
+	import { Toaster } from "$lib/components/ui/sonner";
 
 	let { children } = $props();
 </script>
